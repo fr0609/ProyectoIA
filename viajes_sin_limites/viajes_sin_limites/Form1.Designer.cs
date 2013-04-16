1cfe2f65fe27021aa0e5964c8bee0ab6b67d6140
leonardo castro <leock123@hotmail.com>
2013-04-16 17:04:24 -0600

    combosPaises,CiudadesyAeropuerotos

--- viajes_sin_limites/viajes_sin_limites/Form1.Designer.cs
+++ viajes_sin_limites/viajes_sin_limites/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.Escalas = new System.Windows.Forms.CheckBox();
@@ -37,6 +38,27 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.databaseVSLDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseVSLDataSet1 = new viajes_sin_limites.DatabaseVSLDataSet1();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ciudadesTableAdapter = new viajes_sin_limites.DatabaseVSLDataSet1TableAdapters.CiudadesTableAdapter();
+            this.ciudadesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.paisesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.paisesTableAdapter = new viajes_sin_limites.DatabaseVSLDataSet1TableAdapters.PaisesTableAdapter();
+            this.paisesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseVSLDataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseVSLDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciudadesBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -118,11 +140,139 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Busqueda de Rutas";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.paisesBindingSource;
+            this.comboBox1.DisplayMember = "Nombre";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(415, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.ValueMember = "Codigo";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // databaseVSLDataSet1BindingSource
+            // 
+            this.databaseVSLDataSet1BindingSource.DataSource = this.databaseVSLDataSet1;
+            this.databaseVSLDataSet1BindingSource.Position = 0;
+            // 
+            // databaseVSLDataSet1
+            // 
+            this.databaseVSLDataSet1.DataSetName = "DatabaseVSLDataSet1";
+            this.databaseVSLDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ciudadesTableAdapter
+            // 
+            this.ciudadesTableAdapter.ClearBeforeFill = true;
+            // 
+            // ciudadesBindingSource1
+            // 
+            this.ciudadesBindingSource1.DataMember = "Ciudades";
+            this.ciudadesBindingSource1.DataSource = this.databaseVSLDataSet1BindingSource;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(415, 81);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(415, 124);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(415, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Origen";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DataSource = this.paisesBindingSource1;
+            this.comboBox4.DisplayMember = "Nombre";
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(590, 46);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 18;
+            this.comboBox4.ValueMember = "Codigo";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(590, 80);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 19;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(590, 123);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(590, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Destino";
+            // 
+            // paisesBindingSource
+            // 
+            this.paisesBindingSource.DataMember = "Paises";
+            this.paisesBindingSource.DataSource = this.databaseVSLDataSet1;
+            // 
+            // paisesTableAdapter
+            // 
+            this.paisesTableAdapter.ClearBeforeFill = true;
+            // 
+            // paisesBindingSource1
+            // 
+            this.paisesBindingSource1.DataMember = "Paises";
+            this.paisesBindingSource1.DataSource = this.databaseVSLDataSet1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(738, 497);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -134,6 +284,12 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseVSLDataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseVSLDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciudadesBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +306,22 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private DatabaseVSLDataSet1 databaseVSLDataSet1;
+        private System.Windows.Forms.BindingSource databaseVSLDataSet1BindingSource;
+        private DatabaseVSLDataSet1TableAdapters.CiudadesTableAdapter ciudadesTableAdapter;
+        private System.Windows.Forms.BindingSource ciudadesBindingSource1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.BindingSource paisesBindingSource;
+        private DatabaseVSLDataSet1TableAdapters.PaisesTableAdapter paisesTableAdapter;
+        private System.Windows.Forms.BindingSource paisesBindingSource1;
     }
 }
 
--- viajes_sin_limites/viajes_sin_limites/Form1.cs
+++ viajes_sin_limites/viajes_sin_limites/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace viajes_sin_limites
 {
@@ -31,5 +32,153 @@ namespace viajes_sin_limites
             Rutas d = new Rutas();
             d.getRoute("MMA,", "SJO");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           /* InitializeComponent();
+            conexión.Open();
+            comboBox1_BindingContextChanged();
+            conexión.Close();*/
+        }
+
+
+       /* OleDbConnection conexión = new OleDbConnection(@"PROVIDER=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Quincho\Documents\GitHub\ProyectoIA\viajes_sin_limites\viajes_sin_limites\DatabaseVSL.accdb");
+
+        public void Busqueda()
+        {
+            InitializeComponent();
+            conexión = new OleDbConnection(@"PROVIDER=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Proyecto Abarrotes(Tiendita)\Tiendita.accdb");
+            conexión.Open();
+        }*/
+
+        /*private DataTable GetData()
+        {
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseVSL.accdb");
+            con.Open();
+            //DataSet dt = new DataSet(
+            string comb = comboBox1.SelectedValue.ToString();
+            OleDbDataAdapter dat = new OleDbDataAdapter("SELECT Id, Nombre, codigo_pais FROM Ciudades WHERE codigo_pais='" + comb + "'", con);
+            // MessageBox.Show(SELECT Id, Nombre, codigo_pais FROM Ciudades WHERE codigo_pais="+comb)
+            DataTable dt = new DataTable();
+            dat.Fill(dt);
+            return dt;
+        }*/
+
+
+      // private void comboBox1_BindingContextChanged(/*object sender, EventArgs e*/)
+        /*{
+            try
+            {
+                comboBox1.DataSource = GetData();
+                comboBox1.DisplayMember = "codigo";
+                comboBox1.ValueMember = "id_ciudad";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }   
+        }*/
+
+       private void Form1_Load(object sender, EventArgs e)
+       {
+           // TODO: esta línea de código carga datos en la tabla 'databaseVSLDataSet1.Paises' Puede moverla o quitarla según sea necesario.
+           this.paisesTableAdapter.Fill(this.databaseVSLDataSet1.Paises);
+           // TODO: esta línea de código carga datos en la tabla 'databaseVSLDataSet1.Ciudades' Puede moverla o quitarla según sea necesario.
+           this.ciudadesTableAdapter.Fill(this.databaseVSLDataSet1.Ciudades);
+           // TODO: esta línea de código carga datos en la tabla 'databaseVSLDataSet1.Ciudades' Puede moverla o quitarla según sea necesario.
+           this.ciudadesTableAdapter.Fill(this.databaseVSLDataSet1.Ciudades);
+           // TODO: esta línea de código carga datos en la tabla 'databaseVSLDataSet1.Paises' Puede moverla o quitarla según sea necesario.
+           
+
+       }
+
+
+       private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+       {
+           OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseVSL.accdb");
+           con.Open();
+           string comb = comboBox1.SelectedValue.ToString();
+           OleDbDataAdapter dat = new OleDbDataAdapter("SELECT Id, Nombre, codigo_pais FROM Ciudades WHERE codigo_pais='"+comb+"'", con);
+           DataTable dt = new DataTable();
+           dat.Fill(dt);
+           int len= dt.Rows.Count;
+           for (int i = 0; i < len; i++)
+           {
+               comboBox2.DataSource = dt;
+               comboBox2.DisplayMember = "Nombre";
+               comboBox2.ValueMember = "Id";
+           }
+           con.Close();
+       }
+
+       private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+       {
+           try
+           {
+               OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseVSL.accdb");
+               con.Open();
+               string comb = comboBox2.SelectedValue.ToString();
+               OleDbDataAdapter dat = new OleDbDataAdapter("SELECT Codigo, Nombre, id_ciudad FROM Aeropuertos WHERE id_ciudad =" + comb + "", con);
+               //MessageBox.Show(comb);
+               DataTable dt = new DataTable();
+               dat.Fill(dt);
+               int len = dt.Rows.Count;
+               for (int i = 0; i < len; i++)
+               {
+                   comboBox3.DataSource = dt;
+                   comboBox3.DisplayMember = "Nombre";
+                   comboBox3.ValueMember = "Codigo";
+               }
+               con.Close();
+            }
+            catch(Exception ex){
+                // MessageBox.Show(ex.Message);
+            }
+       }
+
+       private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+       {
+           OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseVSL.accdb");
+           con.Open();
+           string comb = comboBox4.SelectedValue.ToString();
+           OleDbDataAdapter dat = new OleDbDataAdapter("SELECT Id, Nombre, codigo_pais FROM Ciudades WHERE codigo_pais='" + comb + "'", con);
+           DataTable dt = new DataTable();
+           dat.Fill(dt);
+           int len = dt.Rows.Count;
+           for (int i = 0; i < len; i++)
+           {
+               comboBox5.DataSource = dt;
+               comboBox5.DisplayMember = "Nombre";
+               comboBox5.ValueMember = "Id";
+           }
+           con.Close();
+       }
+
+       private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+       {
+           try
+            {
+               OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseVSL.accdb");
+               con.Open();
+               string comb = comboBox5.SelectedValue.ToString();
+               OleDbDataAdapter dat = new OleDbDataAdapter("SELECT Codigo, Nombre, id_ciudad FROM Aeropuertos WHERE id_ciudad =" + comb + "", con);
+               DataTable dt = new DataTable();
+               dat.Fill(dt);
+               int len = dt.Rows.Count;
+               for (int i = 0; i < len; i++)
+               {
+                   comboBox6.DataSource = dt;
+                   comboBox6.DisplayMember = "Nombre";
+                   comboBox6.ValueMember = "Codigo";
+               }
+               con.Close();
+            }
+            catch(Exception ex){
+            }
+       }
+    
+
+
     }
 }
